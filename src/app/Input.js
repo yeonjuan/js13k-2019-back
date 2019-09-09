52ea81bb775f63f39bd3f7da040b2557bd02b0b7
yeonjuan <yeonjuan93@naver.com>
2019-09-09 23:49:12 +0900

    refactor - delete unused

--- src/app/Input.js
+++ src/app/Input.js
@@ -1,28 +1,25 @@
 import {UP, RIGHT, LEFT, DOWN, ENTER} from "./constants";
 
-class Input {
-  constructor () {
-    this._keymap = {
-      38: UP,
-      40: DOWN,
-      37: LEFT,
-      39: RIGHT,
-      13: ENTER
-    };
-    this._onKeyDown = {};
+function Input () {
+  this._keymap = {
+    38: UP,
+    40: DOWN,
+    37: LEFT,
+    39: RIGHT,
+    13: ENTER
+  };
+  this._onKeyDown = {};
 
-    document.addEventListener('keydown', (e) => {
-      e.preventDefault();
-      const key = this._keymap[e.keyCode];
-      this._onKeyDown[key] && this._onKeyDown[key]();
-    });
-  }
-  on ({key, onKeyDown = null}) {
-    if (onKeyDown) {
-      this._onKeyDown[key] = onKeyDown;
-    }
+  document.addEventListener('keydown', (e) => {
+    e.preventDefault();
+    const key = this._keymap[e.keyCode];
+    this._onKeyDown[key] && this._onKeyDown[key]();
+  });
+
+  this.on = ({key, onKeyDown}) => {
+    this._onKeyDown[key] = onKeyDown;
     return this;
-  }
+  };
 }
 
 export default new Input();
