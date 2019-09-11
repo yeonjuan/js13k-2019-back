728fc7cf78283d54e36d5a1334bfd9c2d0c70ff9
yeonjuan <yeonjuan93@naver.com>
2019-09-11 21:00:43 +0900

    refactor - simplify input

--- src/app/Input.js
+++ src/app/Input.js
@@ -1,25 +1,24 @@
 import {UP, RIGHT, LEFT, DOWN, ENTER} from "./constants";
+import {assign} from "./Utils";
 
-function Input () {
-  this._keymap = {
-    38: UP,
-    40: DOWN,
-    37: LEFT,
-    39: RIGHT,
-    13: ENTER
-  };
-  this._onKeyDown = {};
+const KEYMAP = {
+  38: UP,
+  40: DOWN,
+  37: LEFT,
+  39: RIGHT,
+  13: ENTER
+};
 
-  document.addEventListener('keydown', (e) => {
-    e.preventDefault();
-    const key = this._keymap[e.keyCode];
-    this._onKeyDown[key] && this._onKeyDown[key]();
-  });
+let _onKeydown = {};
 
-  this.on = ({key, onKeyDown}) => {
-    this._onKeyDown[key] = onKeyDown;
-    return this;
-  };
+document.addEventListener('keydown', e => {
+  e.preventDefault();
+  const key = KEYMAP[e.keyCode];
+  _onKeydown[key] && _onKeydown[key]();
+});
+
+function input(keyInputs) {
+  assign(_onKeydown, keyInputs);
 }
 
-export default new Input();
+export default input;
