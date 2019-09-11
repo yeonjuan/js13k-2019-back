fcfa8fccc2455a72557443e87d5125bce6d786e5
yeonjuan <yeonjuan93@naver.com>
2019-09-11 21:01:37 +0900

    refactor - rename Scatter members

--- src/app/Scatter.js
+++ src/app/Scatter.js
@@ -9,50 +9,54 @@ class Scatter {
     this.particles = Array
       .from({length: particleNum},
       () => ({
-        radius: randomInRange(minR,maxR),
-        xSpeed: randomInRange(-speed, speed),
-        ySpeed: randomInRange(-speed, speed / 3),
-        time: randomInRange(20, time),
+        r: randomInRange(minR,maxR),
+        xs: randomInRange(-speed, speed),
+        ys: randomInRange(-speed, speed / 3),
+        t: randomInRange(20, time),
         x: 0,
         y: 0,
       }));
   }
+
   init () {
     this.time = 30;
     this.alive = false;
   }
 
   generate(x, y) {
-    if (this.alive) {
+    const {alive, particles} = this;
+    if (alive) {
       return;
     }
     this.alive = true;
-    this.particles.forEach(ptc => {
+    particles.forEach(ptc => {
       ptc.x = x;
       ptc.y = y;
     });
   }
 
   update (time) {
-    if (this.alive) {
+    const {alive, particles} = this;
+    if (alive) {
       this.time -= time;
       if (this.time < 0) {
         this.alive = false;
         this.time = 30;
       }
-      this.particles.forEach(ptc => {
-        ptc.x += ptc.xSpeed;
-        ptc.y += ptc.ySpeed;
+      particles.forEach(ptc => {
+        ptc.x += ptc.xs;
+        ptc.y += ptc.ys;
       });
     }
   }
 
   render (ctx) {
-    if (this.alive) {
-      ctx.fillStyle = this.color;
-      this.particles.forEach(ptc => {
+    const {alive, color, particles} = this;
+    if (alive) {
+      ctx.fillStyle = color;
+      particles.forEach(ptc => {
         ctx.beginPath();
-        ctx.arc(ptc.x, ptc.y, ptc.radius, 0, 2 * Math.PI);
+        ctx.arc(ptc.x, ptc.y, ptc.r, 0, 2 * Math.PI);
         ctx.fill();
       });
     }
