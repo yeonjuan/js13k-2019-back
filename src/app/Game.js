36e41d2cebdd789bb029a2b050ee63f8d502c44e
yeonjuan <yeonjuan93@naver.com>
2019-09-12 21:21:36 +0900

    feat- caching this

--- src/app/Game.js
+++ src/app/Game.js
@@ -2,15 +2,29 @@ import GameMap from './GameMap';
 import getStage from "../assets/maps";
 import Player from './player';
 import Enemy from "./Enemy";
-import Input from './Input';
+import input from './Input';
 import Asset from './Asset';
-import Hud from './Hud';
-import {createCanvas, selectDom} from "./Utils";
+import {createCanvas, selectDom, showDom} from "./Utils";
+
 import {
-  UP, RIGHT, DOWN, LEFT, ENTER,
-  MAP_SIZE, BLOCK_SIZE,
-  GAME_READY, GAME_PLAYING, GAME_OVER, GAME_STAGE_CLEAR, ENEMY_HEAD_SPRITE
+  UP,
+  RIGHT,
+  DOWN,
+  LEFT,
+  ENTER,
+  MAP_SIZE,
+  BLOCK_SIZE,
+  GAME_READY,
+  GAME_PLAYING,
+  GAME_OVER,
+  GAME_STAGE_CLEAR,
+  ENEMY_HEAD_SPRITE,
+  PLAYER_SPRITE,
+  ENEMY_BODY_SPRITE,
+  MOVE_AUDIO,
+  HIT_AUDIO, SHOT_AUDIO, OVER_HUD, CLEAR_HUD, READY_HUD
 } from "./constants";
+import {playBackgroundMusic} from './Music';
 
 class Game {
   constructor() {
@@ -24,75 +38,72 @@ class Game {
     this.novCtx = nvcs.context;
 
     this.nextms = 0;
-    this.input = Input;
+   // this.input = Input;
     this.state = GAME_READY;
-    this.stageNum = 1;
+    this.stageNum = 5;
   }
 
   init () {
-    Asset.loadImage(PLAYER_SPRITE);
-    Asset.loadImage(ENEMY_BODY_SPRITE);
-    Asset.loadImage(ENEMY_HEAD_SPRITE);
-    Asset.loadAudio(MOVE_AUDIO);
-    Asset.loadAudio(HIT_AUDIO);
-    Asset.loadAudio(SHOT_AUDIO);
-
+    Asset.loadImages([PLAYER_SPRITE, ENEMY_HEAD_SPRITE, ENEMY_BODY_SPRITE]);
+    Asset.loadAudios([MOVE_AUDIO, HIT_AUDIO, SHOT_AUDIO]);
+    playBackgroundMusic();
     this.map = new GameMap(this.ctx);
     this.player = new Player(MAP_SIZE / 2 - 16, MAP_SIZE - 32,  this.map);
-    this.input
-      .on({ key: ENTER, onKeyDown: () => {
+    input({
+      [ENTER]: () => {
         switch(this.state) {
           case GAME_READY:
-            Hud.showHud(READY_HUD, false);
+            showDom(READY_HUD, false);
             this.load(this.stageNum);
             this.state = GAME_PLAYING;
-            sequence.play();
+
             break;
           case GAME_PLAYING:
             break;
           case GAME_OVER:
-            Hud.showHud(OVER_HUD, false);\
+            showDom(OVER_HUD, false);
             this.load(this.stageNum);
             this.state = GAME_PLAYING;
             break;
           case GAME_STAGE_CLEAR:
-            Hud.showHud(CLEAR_HUD, false);
+            showDom(CLEAR_HUD, false);
             this.stageNum ++;
             this.load(this.stageNum);
             this.state = GAME_PLAYING;
             break;
         }
-      }})
+      }
+    })
   }
 
   load (num) {
+    const {player, map} = this;
     const stage = getStage(num);
 
-    this.map.load(stage.map);
-    this.player.init(stage.player.x * BLOCK_SIZE, stage.player.y * BLOCK_SIZE);
-    this.enemies = stage.enemies.map(({x, y, cmd}) => new Enemy(x * BLOCK_SIZE, y * BLOCK_SIZE, this.map, cmd, this));
+    map.load(stage.map);
+    player.init(stage.player.x * BLOCK_SIZE, stage.player.y * BLOCK_SIZE);
+    this.enemies = stage.enemies.map(({x, y, cmd, init = null}) => {
+      const enemy =  new Enemy(x * BLOCK_SIZE, y * BLOCK_SIZE, map, cmd, this);
+      if (init) {
+        enemy.initialSetup(init);
+      }
+      return enemy;
+    });
 
-
-    this.input
-      .on({ key: UP, onKeyDown: () => {
-          this.player.move(UP);
-        } })
-      .on({ key: DOWN, onKeyDown: () => {
-        this.player.move(DOWN);
-        }})
-      .on({ key: LEFT, onKeyDown: () => {
-        this.player.move(LEFT)
-        }})
-      .on({ key: RIGHT, onKeyDown: () => {
-        this.player.move(RIGHT)
-        }});
+     input({
+       [UP]: () => player.move(UP),
+       [DOWN]: () => player.move(DOWN),
+       [LEFT]: () => player.move(LEFT),
+       [RIGHT]: () => player.move(RIGHT),
+     });
   }
 
   update (time) {
+    const {player, enemies, state} = this;
     const timeSlice = (time / 1000) * 60;
-    this.player.update(timeSlice);
-    this.enemies.forEach(enemy => enemy.update(timeSlice));
-    this.enemies.every(enemy => !enemy.isAlive) && (this.state === GAME_PLAYING) && this.stageClear();
+    player.update(timeSlice);
+    enemies.forEach(enemy => enemy.update(timeSlice));
+    enemies.every(enemy => !enemy.alive) && (state === GAME_PLAYING) && this.stageClear();
   }
 
   start() {
@@ -100,38 +111,44 @@ class Game {
   }
 
   renderReady (nms) {
-    this.ctx.clearRect(0,0, MAP_SIZE, MAP_SIZE);
-    this.map.renderReady(this.ctx);
-    this.player.render(this.ctx);
+    const {ctx, map, player} = this;
+
+    ctx.clearRect(0,0, MAP_SIZE, MAP_SIZE);
+    map.renderReady(ctx);
+    player.render(ctx);
   }
 
   renderPlaying(nms) {
+    const {ctx, map, player, novCtx, enemies, state, novCanvas} = this;
+
     let time = nms - this.nextms;
     this.nextms = nms;
 
     this.update(time);
-    this.ctx.clearRect(0,0, MAP_SIZE, MAP_SIZE);
-    this.map.render(this.ctx);
-    this.player.render(this.ctx);
-    Hud.renderHp(this.ctx);
-    this.novCtx.clearRect(0, 0, MAP_SIZE, MAP_SIZE);
-    this.novCtx.fillStyle = 'rgba(0,0,0,0.4)';
-    this.novCtx.fillRect(0, 0, MAP_SIZE, MAP_SIZE);
-    this.enemies.forEach(enemy => enemy.draw(this.ctx, this.novCtx));
-    this.ctx.save();
-    this.ctx.globalCompositeOperation = 'darken';
-    this.ctx.drawImage(this.novCanvas, 0, 0);
-    this.ctx.restore();
+    ctx.clearRect(0,0, MAP_SIZE, MAP_SIZE);
 
-    if (this.player.hp <= 0 && this.state === GAME_PLAYING) {
+    map.render(ctx);
+    player.render(ctx);
+
+    novCtx.clearRect(0, 0, MAP_SIZE, MAP_SIZE);
+    novCtx.fillStyle = 'rgba(0,0,0,0.4)';
+    novCtx.fillRect(0, 0, MAP_SIZE, MAP_SIZE);
+    enemies.forEach(enemy => enemy.render(ctx, novCtx));
+    ctx.save();
+    ctx.globalCompositeOperation = 'darken';
+    ctx.drawImage(novCanvas, 0, 0);
+    ctx.restore();
+
+    if (player.hp <= 0 && state === GAME_PLAYING) {
       this.gameOver();
     }
   }
 
   render (nms) {
-    if (this.state === GAME_READY) {
+    const {state} = this;
+    if (state === GAME_READY) {
       this.renderReady(nms);
-    } else if (this.state === GAME_PLAYING || this.state === GAME_OVER || this.state === GAME_STAGE_CLEAR) {
+    } else if (state === GAME_PLAYING || state === GAME_OVER || state === GAME_STAGE_CLEAR) {
       this.renderPlaying(nms);
     }
     window.requestAnimationFrame(this.render.bind(this));
@@ -141,12 +158,12 @@ class Game {
   gameOver() {
     this.state = GAME_OVER;
     this.player.isAlive = false;
-    Hud.showHud(OVER_HUD, true);
+    showDom(OVER_HUD, true);
   }
 
   stageClear () {
     this.state = GAME_STAGE_CLEAR;
-    Hud.showHud(CLEAR_HUD, true);
+    showDom(CLEAR_HUD, true);
   }
 }
 
